731e342abde756836d6c6d69d59683e04f8de0e9
exponent-training <exponent-training@github.com>
2025-03-01 09:33:19 +0530

    All Update And get Operation Done.

--- Hibernate/HibernateOneToManyUniExample/src/com/main/Application.java
+++ Hibernate/HibernateOneToManyUniExample/src/com/main/Application.java
@@ -17,6 +17,9 @@ public class Application {
 		while(true) {
 			System.out.println("1. Add Data User and AddToCart");
 			System.out.println("2. Get Data Using User Id With AddToCart");
+			System.out.println("3. Get All User Data With AddToCart");
+			System.out.println("4. Update User Data ");
+			System.out.println("5. Update Addtocart ");
 
 			int ch = sc.nextInt();
 
@@ -26,6 +29,15 @@ public class Application {
 				break;
 			case 2:
 				op.getUserDataWithAddToCartUsingUserId();
+				break;
+			case 3:
+				op.getAllUserDataWithAddToCart();;
+				break;
+			case 4:
+				op.updateUserDataUsingUserID();
+				break;
+			case 5:
+				op.updateAddToCartUsingUserId();
 				break;
 			default:
 				break;
--- Hibernate/HibernateOneToManyUniExample/src/com/service/operationImpl.java
+++ Hibernate/HibernateOneToManyUniExample/src/com/service/operationImpl.java
@@ -1,9 +1,12 @@
 package com.service;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import com.model.AddToCart;
 import com.model.User;
@@ -12,30 +15,30 @@ import com.util.HibernateUtil;
 public class operationImpl implements OperationI {
 
 	SessionFactory sf = HibernateUtil.getSessionFactory();
-	
-	
+
+
 	@Override
 	public void addingUserWithAddToCartData() {
 		// TODO Auto-generated method stub
 		User user = new User();
 		user.setName("Admin");user.setUname("admin");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("how many product add to cart");
 		int no = sc.nextInt();
-		
+
 		for (int i = 0; i < no; i++) {
-			
+
 			AddToCart addToCart = new AddToCart();
 			addToCart.setProductName("Samsung s23");
 			addToCart.setType("Electronics");
 			addToCart.setPrice(125000);
 			addToCart.setModelNumber("SM12345");
-			
+
 			user.getAddToCart().add(addToCart);
 		}
-		
+
 		Session session = sf.openSession();
 		session.save(user);
 		session.beginTransaction().commit();
@@ -45,33 +48,108 @@ public class operationImpl implements OperationI {
 	@Override
 	public void getUserDataWithAddToCartUsingUserId() {
 		// TODO Auto-generated method stub
-		
+		Session session = sf.openSession();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your id : ");
+		int id = sc.nextInt();
+
+		User user = session.get(User.class, id);
+		//		if(!Objects.isNull(user)) {
+		//			System.out.println(user.getName());
+		//		}else {
+		//			System.out.println("User Does not exists.");
+		//		}
+		if(user != null) {
+			System.out.println(user.getName());
+		} else {
+			System.out.println("User Does not exists.");
+		}
 	}
 
 	@Override
 	public void getAllUserDataWithAddToCart() {
 		// TODO Auto-generated method stub
-		
+        Session session = sf.openSession();
+        Query<User> query = session.createQuery("from User", User.class);
+        List<User> ulist = query.getResultList();
+        System.out.println(ulist);
 	}
 
 	@Override
 	public void updateUserDataUsingUserID() {
 		// TODO Auto-generated method stub
-		
+		Session session = sf.openSession();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your id : ");
+		int id = sc.nextInt();
+
+		User user = session.get(User.class, id);
+		//		if(!Objects.isNull(user)) {
+		//			System.out.println(user.getName());
+		//		}else {
+		//			System.out.println("User Does not exists.");
+		//		}
+		if(user != null) {
+			System.out.println(user.getName());
+			user.setName("Ajas");
+			user.setUname("ajas.shaikh");
+			
+			session.update(user);
+			
+			session.beginTransaction().commit();
+		} else {
+			System.out.println("User Does not exists.");
+		}
 	}
 
 	@Override
 	public void updateAddToCartUsingUserId() {
 		// TODO Auto-generated method stub
-		
+		Session session = sf.openSession();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your id : ");
+		int id = sc.nextInt();
+
+		User user = session.get(User.class, id);
+		if(user != null) {
+			System.out.println(user.getName());
+		   
+			List<AddToCart> list = user.getAddToCart();
+		    
+			if(list.size() > 0) {
+				
+				for(AddToCart ad : list) {
+					if(ad.getModelNumber() != null) {
+						System.out.println("Please provide Model number : ");
+						String modelnumber = sc.next();
+						if(modelnumber.equals(ad.getModelNumber())) {
+							ad.setProductName("Apple 16pro");
+							ad.setPrice(120000);
+							ad.setType("Electrionics");
+							break;
+						}
+					}
+				}
+//				for(int i=0; i < list.size();i++) {
+//					AddToCart ad = list.get(i);
+//				}
+
+			} else {
+				System.out.println("Add to cart is empty.");
+			}
+		      session.update(user);
+		      session.beginTransaction().commit();
+		} else {
+			System.out.println("User Does not exists.");
+		}
 	}
 
 	@Override
 	public void deleteAddToCartUsingUserId() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
+
 
 }
